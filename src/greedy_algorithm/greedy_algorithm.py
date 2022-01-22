0c2931441b156ab04d6612c5ff6e064bffd29af4
Wojciech Moczydlowski <wojciech.moczydlowski@leancode.pl>
2022-01-22 19:45:12 +0100

    Implement greedy algorithm

---
+++ src/greedy_algorithm/greedy_algorithm.py
@@ -0,0 +1,58 @@
+import random
+
+from typing import List
+
+from src.utils.child import Child
+
+
+def greedy_algorithm(children: List[Child], iterations: int):
+    random.seed(None)
+
+    children_amount = len(children)
+
+    for i in range(iterations):
+        child_index = random.randrange(0, children_amount)
+
+        child = children[child_index]
+        prev_child = children[child_index - 1] if child_index - 1 >= 0 else None
+        next_child = children[child_index + 1] if child_index + 1 < children_amount else None
+
+        if prev_child is None:
+            child.set_cookies(count_cookies_for_two_children(child, next_child))
+
+        elif next_child is None:
+            child.set_cookies(count_cookies_for_two_children(child, prev_child))
+        else:
+            child.set_cookies(count_cookies_for_three_children(child, prev_child, next_child))
+
+    return children
+
+def count_cookies_for_two_children(child, second_child):
+    if child.mark > second_child.mark:
+        return second_child.cookies + 1
+    elif child.mark < second_child.mark:
+        return 1
+    else:
+        return second_child.cookies
+
+
+def count_cookies_for_three_children(child, prev_child, next_child):
+    if child.mark < prev_child.mark and child.mark < next_child.mark:
+        return 1
+    elif child.mark > prev_child.mark and child.mark > next_child.mark:
+        if prev_child.mark > next_child.mark:
+            return prev_child.cookies + 1
+        elif prev_child.mark < next_child.mark:
+            return next_child.cookies + 1
+        else:
+            return next_child.cookies
+    else:
+        if prev_child.mark == child.mark:
+            return prev_child.cookies
+        elif next_child.mark == child.mark:
+            return next_child.cookies
+        else:
+            if prev_child.mark > next_child.mark:
+                return next_child.cookies + 1
+            else:
+                return prev_child.cookies + 1
--- src/utils/models.py
+++ src/utils/models.py
@@ -6,14 +6,17 @@ class Child:
         self.mark = mark
         self.cookies = cookies
 
+    def __repr__(self):
+        return f"Child with m:{self.mark} and c:{self.cookies}"
+
     def give_cookie(self):
         self.cookies = self.cookies + 1
 
     def take_cookie(self):
         self.cookies = self.cookies - 1
 
-    def __repr__(self):
-        return f"Child with m:{self.mark} and c:{self.cookies}"
+    def set_cookies(self, cookies):
+        self.cookies = cookies
 
 
 Children = List[Child]
