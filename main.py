461c7c8473521322bb95c9366b64db6eca2a57d1
Jan Piotrowski <janfpiotrowski@gmail.com>
2022-01-22 17:33:37 +0100

    add models.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from src.evolutionary_algorithm.evolutionary_algorithm import solve_with_evolutionary_algorithm
+from src.evolutionary_algorithm.models import EvolutionaryAlgorithmParameters
 from src.generator.generate_date import generate_data
 
 seed = 100
 amount = 100
 
 data = generate_data(seed, amount)
-print(data)
+print(solve_with_evolutionary_algorithm(data, EvolutionaryAlgorithmParameters()))
---
+++ src/evolutionary_algorithm/evolutionary_algorithm.py
@@ -0,0 +1,9 @@
+from typing import List
+
+from src.evolutionary_algorithm.models import EvolutionaryAlgorithmParameters
+from src.utils.child import Child
+
+
+def solve_with_evolutionary_algorithm(children: List[Child], parameters: EvolutionaryAlgorithmParameters):
+    return children
+
